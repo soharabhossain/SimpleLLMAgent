88d3cd8dc1e294c7429fb8a3d6151f0c34ec03dc
Soharab <40207326+soharabhossain@users.noreply.github.com>
2025-04-14 19:15:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='simple_llm_agent',
     version='0.1',
+    description="A simple LLM-based agent",
+    author="Soharab Hossain Shaikh",
     packages=find_packages(),
 )
