10ff98eca084cb9e000b645d72826d78640c8fdf
Soharab <40207326+soharabhossain@users.noreply.github.com>
2025-06-06 16:45:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ from agent.base import LLMAgent
 from agent.tools import CalculatorTool
 from config import run_config
 
+# --------------------------------------------------------------------------------------
 if __name__ == "__main__":
-
+# --------------------------------------------------------------------------------------
     llm_provider="groq"
-
     run_config(llm_provider=llm_provider)
 
     tools = [CalculatorTool()]
     agent = LLMAgent(name="SimpleGPTAgent", tools=tools, llm_provider=llm_provider)
+    print("--------------------------------------------\n")
 
+# --------------------------------------------------------------------------------------
     # query1 = "What is the capital of France?"
     query1 = "Briefly tell me about the Italian movie Malena."
-
-    query2 = "What is 12 * 9?"
-    # query2 = "What is 2 ^ 9?"
-
-    query3 = "What is the statement for Newton's the second law of motion?"
-
     print("User:", query1)
     print("Agent:", agent.run(query1))
+    print("--------------------------------------------\n")
 
-    # print("\nUser:", query2)
-    # print("Agent:", agent.run(query2))
+# --------------------------------------------------------------------------------------
+    # query2 = "What is 12 * 9?"
+    query2 = "What is 2 ^ 9**55%5?"
+    print("\nUser:", query2)
+    print("Agent:", agent.run(query2))
+    print("--------------------------------------------\n")
 
-    # print("\nUser:", query3)
-    # print("Agent:", agent.run(query3))
+# --------------------------------------------------------------------------------------
+    query3 = "What is the statement for Newton's the second law of motion?"
+    print("\nUser:", query3)
+    print("Agent:", agent.run(query3))
+    print("--------------------------------------------\n")
+
+# --------------------------------------------------------------------------------------
